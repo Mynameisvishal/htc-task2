5e257aa9d6edc9a6320dd21fd013c161de314cd8
Vishal P <vishalprakaash@gmail.com>
2021-04-30 15:41:22 +0530

    style added

--- src/Post.js
+++ src/Post.js
@@ -1,6 +1,7 @@
 import React,{useEffect} from 'react'
 import {useSelector,useDispatch} from 'react-redux';
 import { fetchPost } from './actions';
+import './Post.css';    
 
 const Post = () => {
 
@@ -8,16 +9,19 @@ const Post = () => {
     const dispatch = useDispatch();
 
     useEffect(() => {
-        console.log("inside useEffect post");
         dispatch(fetchPost())
     }, [])
 
     return (
         <div>
             {console.log(state)}
+            <div className="container m-5">
+            <h4>Api fetch using redux thunk</h4>
             {state.items?.map((data,key)=>(
                 <p id={key}> {data.name} </p>
-            ))}
+                ))}
+                <p style={{minHeight: '50px'}}></p>
+                </div>
         </div>
     )
 }
